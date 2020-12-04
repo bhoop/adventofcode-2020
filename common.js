f6932b505b0c0bcf0d5a01cf6270ae94ea11b72f
bhoop <briley@gmail.com>
2020-12-04 16:55:01 -0500

    keep empty lines in input

--- common.js
+++ common.js
@@ -1,10 +1,10 @@
-var fs = require('fs');
+var fs = require("fs");
 
 exports.getInput = function getInput(asNumbers = false) {
-    let lines = process.stdin.isTTY
-        ? process.argv.slice(2)
-        : fs.readFileSync( 0, 'utf-8' ).split(/[\r\n]+/g);
-    
-    if ( asNumbers ) lines = lines.map( Number ).filter(n => !!n);
-    return lines;
-}
+	let lines = process.stdin.isTTY
+		? process.argv.slice(2)
+		: fs.readFileSync(0, "utf-8").split(/\r?\n/g);
+
+	if (asNumbers) lines = lines.map(Number).filter((n) => !!n);
+	return lines;
+};
